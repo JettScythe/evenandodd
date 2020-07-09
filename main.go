618d568b480bbd9c7a2a72233997032b4d38da4d
JettScythe <jettscythe@hotmail.com>
2020-07-09 03:00:38 -0300

    Better variable Names

--- main.go
+++ main.go
@@ -4,11 +4,11 @@ import "fmt"
 
 func main() {
 	nums := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	for _, even := range nums {
-		if even%2 == 0 {
-			fmt.Printf("%v is even\n", even)
+	for _, num := range nums {
+		if num%2 == 0 {
+			fmt.Printf("%v is even\n", num)
 		} else {
-			fmt.Printf("%v is odd\n", even)
+			fmt.Printf("%v is odd\n", num)
 		}
 	}
 }
